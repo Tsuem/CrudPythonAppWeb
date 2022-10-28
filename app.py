aa3de6c2b5d045b40e35a8628217d55ff6590713
tsuem <tsuemotop@gmail.com>
2022-10-27 23:32:13 -0300

    eliminando datos de la tabla

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
-from flask import render_template, request
+from flask import render_template, request, redirect
 from flaskext.mysql import MySQL
+from datetime import datetime
 
 #se crea la app
 app = Flask(__name__)
@@ -16,17 +17,53 @@ mysql.init_app(app)
 @app.route('/')
 def index():
 
-    sql = "INSERT INTO `empleados` (`id`, `nombre`, `correo`, `foto`) VALUES (NULL, 'Nako', 'nakogatis@gmail.com', 'foto.jpg');"
+    sql = "SELECT * FROM `empleados`;"
     conn = mysql.connect()
     cursor = conn.cursor()
     cursor.execute(sql)
+
+    empleados = cursor.fetchall()
+    print(empleados)
+
     conn.commit()
+    return render_template('empleados/index.html', empleados = empleados)
+
+@app.route('/destroy/<int:id>')
+def destroy(id):
+    conn = mysql.connect()
+    cursor=conn.cursor()
+
+    cursor.execute("DELETE FROM empleados WHERE id= %s",(id))
+    conn.commit()
+    return redirect('/')
     
-    return render_template('empleados/index.html')
 
 @app.route('/create')
 def create():
     return render_template('empleados/create.html')
+
+@app.route('/store', methods=['POST'])
+def storage():
+    _nombre=request.form['txtNombre']
+    _correo=request.form['txtCorreo']
+    _foto=request.files['txtFoto']
+    
+    now= datetime.now()
+    tiempo=now.strftime("%Y%H%M%S")
+
+    if _foto.filename != '':
+        nuevoNombreFoto = tiempo+_foto.filename
+        _foto.save("uploads/"+nuevoNombreFoto)
+
+    sql = "INSERT INTO `empleados` (`id`, `nombre`, `correo`, `foto`) VALUES (NULL, %s, %s, %s);"
+
+    datos=(_nombre, _correo, _foto.filename)
+
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute(sql, datos)
+    conn.commit()
+    return render_template('empleados/index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)

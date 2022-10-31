8ddfc036f6c0819faacfe618e1411178e047b053
tsuem <tsuemotop@gmail.com>
2022-10-31 02:15:41 -0300

    aplicacion web con python terminada

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask
-from flask import render_template, request, redirect
+from flask import render_template, request, redirect, url_for, flash
 from flaskext.mysql import MySQL
+from flask import send_from_directory
+
 from datetime import datetime
+import os
+
 
 # se crea la app
 app = Flask(__name__)
+app.secret_key ="Develoteca"
 
 mysql = MySQL()
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
@@ -13,6 +18,12 @@ app.config['MYSQL_DATABASE_PASSWORD'] = ''
 app.config['MYSQL_DATABASE_DB'] = 'sistema'
 mysql.init_app(app)
 
+CARPETA = os.path.join('uploads')
+app.config['CARPETA'] = CARPETA
+
+@app.route('/uploads/<nombreFoto>')
+def uploads(nombreFoto):
+    return send_from_directory(app.config['CARPETA'], nombreFoto)
 
 @app.route('/')
 def index():
@@ -33,6 +44,10 @@ def index():
 def destroy(id):
     conn = mysql.connect()
     cursor = conn.cursor()
+
+    cursor.execute("SELECT foto FROM empleados WHERE id=%s", (id))
+    fila=cursor.fetchall()
+    os.remove(os.path.join(app.config['CARPETA'], fila[0][0]))
 
     cursor.execute("DELETE FROM empleados WHERE id= %s", (id))
     conn.commit()
@@ -64,6 +79,21 @@ def update():
 
     conn = mysql.connect()
     cursor = conn.cursor()
+
+    now = datetime.now()
+    tiempo = now.strftime("%Y%H%M%S")
+
+    if _foto.filename != '':
+        nuevoNombreFoto = tiempo+_foto.filename
+        _foto.save("uploads/"+nuevoNombreFoto)
+
+        cursor.execute("SELECT foto FROM empleados WHERE id=%s", id)
+        fila=cursor.fetchall()
+
+        os.remove(os.path.join(app.config['CARPETA'], fila[0][0]))
+        cursor.execute("UPDATE empleados SET foto=%s WHERE id=%s", (nuevoNombreFoto, id))
+        conn.commit()
+
     cursor.execute(sql, datos)
     conn.commit()
 
@@ -81,6 +111,10 @@ def storage():
     _correo = request.form['txtCorreo']
     _foto = request.files['txtFoto']
 
+    if _nombre=='' or _correo=='' or _foto=='':
+        flash('Recuerda llenar los datos de los campos')
+        return redirect(url_for('create'))
+
     now = datetime.now()
     tiempo = now.strftime("%Y%H%M%S")
 
@@ -96,7 +130,7 @@ def storage():
     cursor = conn.cursor()
     cursor.execute(sql, datos)
     conn.commit()
-    return render_template('empleados/index.html')
+    return redirect('/')
 
 
 if __name__ == '__main__':
